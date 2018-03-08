acf89071a514525dab88e15d4eb7526a04417b4d
pinkal-bacancy <pinkal.darji@bacancytechnology.com>
2018-03-08 16:03:00 +0530

    demo of omniauth gem

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'home/show'
+
+ get   '/login', :to => 'sessions#new', :as => :login
+ get '/auth/google_oauth2/callback', :to => 'sessions#create'
+  get '/auth/failure', :to => 'sessions#failure'
+  get '/logout', :to => 'sessions#destroy'
+  resource :home, only: [:show]
+
+  root to: "home#show"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
