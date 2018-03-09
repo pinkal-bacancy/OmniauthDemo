a9849e804cff315937b65b7b4d1e672fd3d75514
pinkal-bacancy <pinkal.darji@bacancytechnology.com>
2018-03-09 15:00:24 +0530

    changes in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,21 @@ class SessionsController < ApplicationController
 	end
 	def create
 		auth_hash = request.env['omniauth.auth']
+    puts "-----------------------"
+    puts auth_hash
 
 		if session[:user_id]
     # Means our user is signed in. Add the authorization to the user
-    User.find(session[:user_id]).add_provider(auth_hash)
+    user=User.find(session[:user_id]).add_provider(auth_hash)
+    puts "---------------user---------------"
+    puts user
 
     render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
   else
     # Log him in or sign him up
     auth = Authorization.find_or_create(auth_hash)
+    puts "------------auth-----------------"
+    puts auth
 
     # Create the session
     session[:user_id] = auth.user.id
