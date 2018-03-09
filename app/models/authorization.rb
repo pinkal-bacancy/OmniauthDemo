051e8e4e42c23adf56e44afd56a6a1a9bfb1b15f
pinkal-bacancy <pinkal.darji@bacancytechnology.com>
2018-03-09 15:42:16 +0530

    changes in authorization model

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -6,7 +6,7 @@ class Authorization < ApplicationRecord
   	unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
     	puts "---------------------unless----------------------"
     	a=auth_hash["user_info"]["name"]
-    	puts a
+    	puts "#{a}"
     	user = User.create :name => auth_hash["user_info"]["name"], :email => auth_hash["user_info"]["email"]
     	puts "--------------------user------------"
 
