a715c647eb754c951314c27db7a795601501dab5
pinkal-bacancy <pinkal.darji@bacancytechnology.com>
2018-03-13 13:02:05 +0530

    change signout path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,24 +6,24 @@ class SessionsController < ApplicationController
         puts "-----------------------"
         puts auth_hash
 
-		if session[:user_id]
+        if session[:user_id]
     # Means our user is signed in. Add the authorization to the user
-        user=User.find(session[:user_id]).add_provider(auth_hash)
-     
-        render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
+          user=User.find(session[:user_id]).add_provider(auth_hash)
+
+          render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
         else
     # Log him in or sign him up
-    auth = Authorization.find_or_create(auth_hash)
-    puts "------------auth-----------------"
-    puts auth.user.name
+          auth = Authorization.find_or_create(auth_hash)
+          puts "------------auth-----------------"
+          puts auth.user.name
 
     # Create the session
-    session[:user_id] = auth.user.id
-    redirect_to root_path, :notice => "Signed in!"
-      end
-end
-def destroy
-	session[:user_id] = nil
-  redirect_to root_url, :notice => "Signed out!"end
-
+          session[:user_id] = auth.user.id
+          redirect_to root_path, :notice => "Signed in!"
+        end
+  end
+  def destroy
+	  session[:user_id] = nil
+    redirect_to root_url, :notice => "Signed out!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
  get   '/login', :to => 'sessions#new', :as => :login
  get '/auth/google_oauth2/callback', :to => 'sessions#create'
-  get '/logout', :to => 'sessions#destroy'
+ 
   resource :home, only: [:show]
 
   root to: "sessions#new"
